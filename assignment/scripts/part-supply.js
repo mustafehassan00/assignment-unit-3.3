4d236928aa56cf7108488257e358b6882d172603
Mustafe Hassan <mustafeh123@gmail.com>
2024-03-10 17:17:46 -0500

    Did question 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -42,8 +42,23 @@ console.log(supplyChanges);
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
 
-
-
+let positives=[];
+let negatives=[];
+let zeroes=[];
+for (let i = 0; i < supplyChanges.length; i++) {
+if(supplyChanges[i] > 0){
+  positives.push(supplyChanges[i]);
+}
+else if(supplyChanges[i] < 0){
+  negatives.push(supplyChanges[i]);
+}
+else {
+  zeroes.push(supplyChanges[i]);
+}
+}
+console.log('Zeroes', zeroes);
+console.log('Positives',positives);
+console.log('Negatives', negatives);
 
 // ***** STRETCH GOALS *********************************************
 // 7. Rewrite the 'for' loop from #6 as a 'for...of' loop. Instead of 'positives',
